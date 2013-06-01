9b11bdd421f8f9024830573bc8327fb712eea515
Saket Srivastava <srivastav.saket@gmail.com>
2013-06-01 15:50:14 +0530

    Symbol Bug Fix

--- core.h
+++ core.h
@@ -4,14 +4,20 @@
 #include <conio.h>
 #include <string.h>
 wchar_t pass[100];
+int symch[100],symchcou=0;
+bool strcontain(int check)
+{
+	for(int i=0;i<symchcou;i++)
+	{
+		if(symch[i]==check)
+			return true;
+	}
+	return false;
+}
 wchar_t * core(int got,int nget,int sget,char nset[],char set[],int tim,char sym[])
 {
+	symchcou=0;
 	int counter = 0,a,num,nnum,nsym,deb=0,b=0,icou=0;
-	while(deb<100)
-	{
-		pass[deb]=NULL;
-		deb++;
-	};
 	num = strlen(set);
 	num--;
 	nsym = strlen(sym);
@@ -39,24 +45,17 @@ wchar_t * core(int got,int nget,int sget,char nset[],char set[],int tim,char sym
 	counter=0;
 	while(counter<sget)
 	{
-		again:
 		a=rand()%got;
-		while(pass[a]=='1'||pass[a]=='2'||pass[a]=='3'||pass[a]=='4'||pass[a]=='5'||pass[a]=='6'||pass[a]=='7'||pass[a]=='8'||pass[a]=='9'||pass[a]=='0')
+		while(pass[a]=='1'||pass[a]=='2'||pass[a]=='3'||pass[a]=='4'||pass[a]=='5'||pass[a]=='6'||pass[a]=='7'||pass[a]=='8'||pass[a]=='9'||pass[a]=='0'||strcontain(a))
 		{
 			a=rand()%got;
 		};
-		icou=0;
-		while(icou<=got)
-		{
-			if(pass[a]==sym[icou])
-			{
-				goto again;
-			};
-			icou++;
-		};
 		b=rand()%nsym;
 		pass[a]=sym[b];
+		symch[symchcou]=a;
+		symchcou++;
 		counter++;
 	};
+	pass[got]=0;
     return pass;
 }
